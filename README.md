d9dce58966f618299c22353185a12780dfdceecd
Scott Corgan <scott@divshot.com>
2013-06-18 15:19:10 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-bower-s3
-========
+# bower-s3
 
 Install bower packages directly to s3 buckets.
+
+## Install
+
+```
+npm install bower-s3 --save
+```
+
+## Usage
+
+```javascript
+var BowerS3 = require('bower-s3');
+
+var options = {
+  key: 'some-aws-key',
+  secret: 'some-aws-secret-key',
+  bucket: 'bower-s3',
+  componentDirectory: 'components' // optional
+  region: 'US_EAST_1' // optional
+};
+
+var bowerS3 = new BowerS3(options);
+
+bowerS3.install(['jquery'], function (err) {
+  //
+});
+```
+
+## Todo
+
+* Provide a streaming interface as well as callback
+* Find a way to stream straight form Bower to S3. Currently we download the files and stream them to S3
+
+## Changelog
+
+* **0.3.0**
+  * NEW - Install component by component name rather than component repo
+  * NEW - Options for region and default component folder
+  * FIXED - Refactor to use streams internally
+  * FIXED - Remove uneccessary files.
+
+* **0.0.1**
+  * Inital download from bower and stream/put to S3
+  * Lots of dirty code
--- bower-s3.js
+++ bower-s3.js
@@ -1,25 +1,11 @@
 var path = require('path');
+var assert = require('assert');
 var bower = require('bower');
 var hat = require('hat');
 var fs = require('fs-extra');
-var walk = require('walk')
 var amazonS3 = require('awssum-amazon-s3');
-
-
-
-
-/*
-  - bucket name
-  - destination directory name (default: components)
-  - bower package name,
-  - accessKey
-  - secretKey
-  
-  DEFAULTS:
-  'accessKeyId'     : 'AKIAJZLSGG5WYIXWVQFA',
-  'secretAccessKey' : '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',
-  'region'          : amazonS3.US_EAST_1
- */
+var streamDir = require('stream-dir');
+var through = require('through');
 
 var DEFAULT_COMPONENT_DIRECTORY = 'components';
 var DEFAULT_AWS_REGION = amazonS3.US_EAST_1;
@@ -29,6 +15,7 @@ var BowerS3 = function (options) {
   assert.notEqual(options.secret, undefined, 'Requres S3 AWS Secret');
   assert.notEqual(options.bucket, undefined, 'Requires AWS S3 bucket name.');
   
+  this.bucket = options.bucket;
   this.componentDirectory = options.componentDirectory || DEFAULT_COMPONENT_DIRECTORY;
   this.region = options.region || DEFAULT_AWS_REGION;
   this._s3 = new amazonS3.S3({
@@ -38,52 +25,7 @@ var BowerS3 = function (options) {
   });
 };
 
-BowerS3.prototope.install = function (components, callback) {
-  // Always need it as an array
-  if (typeof components === 'string') {
-    components = [compontents];
-  }
-};
-
-
-module.exports = BowerS3;
-
-return;
-
-
-// var bowerS3 = new BowerS3({
-//   key: 'AKIAJZLSGG5WYIXWVQFA',
-//   secret: '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',
-//   bucket: 'bower-s3',
-//   componentDirectory: 'components'
-// });
-
-// bowerS3.install(['asasdfasdfasdfasf.git'], function (err, res) {
-  
-// });
-
-
-
-
-
-
-
-
-var s3 = new amazonS3.S3({
-  'accessKeyId'     : 'AKIAJZLSGG5WYIXWVQFA',
-  'secretAccessKey' : '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',
-  'region'          : amazonS3.US_EAST_1
-});
-
-
-getFromBower(['git://github.com/scottcorgan/xortable.git'], function (err, installDir) {
-  walker = walk.walk(installDir, {followLinks: false});
-  walker.on('file', uploadFileToBucket('bower-s3', 'components', installDir));
-  walker.on('end', rmTempDir(installDir));
-});
-
-
-function getFromBower(components, callback) {
+BowerS3.getFromBower = function(components, callback) {
   var installDir = path.join('.tmp', hat());
   
   fs.mkdirs(installDir, function (err) {
@@ -92,37 +34,77 @@ function getFromBower(components, callback) {
       on('end', function (data) {
        callback(null, installDir);
       }).
-      on('error', function (err) {
-        callback(null);
-      });
+      on('error', callback);
   });
-}
+};
 
-function uploadFileToBucket (bucketname, rootBucketFolder, installDir) {
-  return function (root, fileStats, next) {
-    var relativeRoot = root.replace(installDir, '');
-    var srcPath = path.join(root, fileStats.name);
-    var destPath = path.join(relativeRoot, fileStats.name);
-    console.log(destPath);
+BowerS3.prototype.install = function (components, callback) {
+  var self = this;
+  var componentDirectory = this.componentDirectory;
+  
+  // Always need it as an array
+  if (typeof components === 'string') {
+    components = [components];
+  }
+  
+  BowerS3.getFromBower(components, function (err, installDir) {
+    if (err) {
+      return callback(err);
+    }
     
+    streamDir(installDir).
+      pipe(self.getFileStats).
+      pipe(self.uploadFileToBucketFrom(installDir)).
+      pipe(self.removeInstallDir(installDir)).
+      on('end', callback);
+  });
+};
+
+BowerS3.prototype.uploadFileToBucketFrom = function (installDir) {
+  var self = this;
+  
+  var stream = through(function (fileStats) {
+    var emit = this.emit;
+    var destPath = fileStats.filePath.replace(installDir + '/', '');
     var options = {
-      BucketName    : bucketname,
-      ObjectName    : path.join(rootBucketFolder, destPath),
+      BucketName    : self.bucket,
+      ObjectName    : path.join(self.componentDirectory, destPath),
       ContentLength : fileStats.size,
-      Body          : fs.createReadStream(srcPath)
+      Body          : fs.createReadStream(fileStats.filePath)
     };
     
-    s3.PutObject(options, function(err, data) {
-      next();
+    self._s3.PutObject(options, function(err, data) {
+      emit('data', destPath);
     });
-  };
+  }, function () {
+    this.emit('end');
+  });
+  
+  return stream;
+};
+
+BowerS3.prototype.removeInstallDir = function (installDir) {
+  var stream = through(function () {
+    this.emit('data');
+  }, function () {
+    fs.remove(installDir, function (err) {
+      this.emit('end', null);
+    }.bind(this))
+  });
+  
+  return stream;
 }
 
-function rmTempDir (dir) {
-  return function (err) {
-    fs.remove(dir, function () {
-      console.log('done uploaded');
-    });
-  };
-}
+BowerS3.prototype.getFileStats = through(function (fileName) {
+  fs.stat(fileName, function (err, stats) {
+    stats.filePath = fileName;
+    this.emit('data', stats);
+  }.bind(this));
+}, function (){
+  this.emit('end');
+});
+
+
+module.exports = BowerS3;
+
 
